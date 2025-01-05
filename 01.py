45ea610123eb6ffe97221eccfa1a3e484a24580b
reyr3v4cud83guy <153989521+reyr3v4cud83guy@users.noreply.github.com>
2025-01-05 21:58:15 +0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+print("Welcome to my 100 days of code challenge.")
+print("I'm ABDULLAHI ABDULLAHI, and I'm excited to start this journey.")
+print("Over the next 100 days, I'll be learning and coding every day.")
+print("I'll be sharing my progress and experiences along the way.")
+print("Thanks for joining me on this journey!") 
+
